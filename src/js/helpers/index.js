8984d4d630ca83c1303d7e7206821b29d2e9f69c
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2022-01-31 10:18:01 +0200

    refactor: imporove variables names for helper

--- src/js/helpers/index.js
+++ src/js/helpers/index.js
@@ -1,30 +1,28 @@
 import { cart } from "../main";
 import createElement from "../helpers/createElement"
 
-let total = 0;
-
 function createProductItem(product) {
-    const item = createElement('div', { className: 'products__item' });
-    const productContent = createElement('div', { className: 'products__content' })
-    const img = createElement('img', { className: 'products__img', srcset: product['image'] })
-    const productsDescription = createElement('div', { className: 'products__descr', innerHTML: product['description'] })
-    const productTitle = createElement('div', { className: 'products__title', innerHTML: product['title'] })
-    const productSelling = createElement('div', { className: 'products__selling' })
-    const productPrice = createElement('div', { className: 'products__price', innerHTML: product['price'] + '$' })
-    const addToCart = createElement('button', { className: 'btn add-to-cart', innerHTML: 'Add to cart' })
-    addToCart.addEventListener('click', function () {
+    const productEl = createElement('div', { className: 'products__item' });
+    const contentEl = createElement('div', { className: 'products__content' })
+    const imgEl = createElement('img', { className: 'products__img', srcset: product['image'] })
+    const descriptionEl = createElement('div', { className: 'products__descr', innerHTML: product['description'] })
+    const titleEl = createElement('div', { className: 'products__title', innerHTML: product['title'] })
+    const sellingEl = createElement('div', { className: 'products__selling' })
+    const priceEl = createElement('div', { className: 'products__price', innerHTML: product['price'] + '$' })
+    const addToCartBtnEl = createElement('button', { className: 'btn add-to-cart', innerHTML: 'Add to cart' })
+    addToCartBtnEl.addEventListener('click', function () {
         cart.addItem(product) // TODO it should work
     })
 
-    item.appendChild(productContent)
-    productContent.appendChild(img)
-    productContent.appendChild(productTitle)
-    productContent.appendChild(productsDescription)
-    item.appendChild(productSelling)
-    productSelling.appendChild(productPrice)
-    productSelling.appendChild(addToCart)
+    contentEl.appendChild(imgEl)
+    contentEl.appendChild(titleEl)
+    contentEl.appendChild(descriptionEl)
+    productEl.appendChild(contentEl)
+    productEl.appendChild(sellingEl)
+    sellingEl.appendChild(priceEl)
+    sellingEl.appendChild(addToCartBtnEl)
 
-    return item
+    return productEl
 }
 
 
